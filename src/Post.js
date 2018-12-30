4f46bb70f1998b43fbd53a2828e3ef269aea8b1a
Ruargh <ruargh@gmail.com>
2018-12-31 00:41:30 +0200

    initial commit2

---
+++ src/Post.js
@@ -0,0 +1,12 @@
+// Post.js
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return <p>Home</p>;
+  }
+}
+
+export default Home;
+
+// then save as > Post.js with file updated accordingly
