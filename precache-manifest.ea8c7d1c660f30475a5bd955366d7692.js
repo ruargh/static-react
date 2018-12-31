82b284fb01f73d92ec989e2b705a8e350e3f0745
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 00:14:06 +0000

    Deploy ruargh/static-react to github.com/ruargh/static-react.git:gh-pages

---
+++ precache-manifest.ea8c7d1c660f30475a5bd955366d7692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a31c7b4a09a73f60bf2d",
+    "url": "/user/deployment/s3/static/css/main.5e812e2f.chunk.css"
+  },
+  {
+    "revision": "a31c7b4a09a73f60bf2d",
+    "url": "/user/deployment/s3/static/js/main.a31c7b4a.chunk.js"
+  },
+  {
+    "revision": "819e8ebf203bc4b33c9b",
+    "url": "/user/deployment/s3/static/js/1.819e8ebf.chunk.js"
+  },
+  {
+    "revision": "69246e77be4e8c1cf0fe",
+    "url": "/user/deployment/s3/static/js/runtime~main.69246e77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/user/deployment/s3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d871cfd0513c40174e630873a5b2055",
+    "url": "/user/deployment/s3/index.html"
+  }
+];
